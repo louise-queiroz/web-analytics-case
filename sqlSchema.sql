f1f99246f3d6e5a4dc72cd9bda43d0efdc98d7cf
louise-queiroz <louisequeiroz8@gmail.com>
2024-01-20 20:32:02 -0300

    done

---
+++ sqlSchema.sql
@@ -0,0 +1,19 @@
+-- TABLE
+CREATE TABLE 'dados1' (c1 TEXT,c2 INTEGER,c3 INTEGER,c4 INTEGER,c5 TEXT);
+CREATE TABLE 'dados2' (c1 INTEGER,c2 TEXT);
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE veiculos(
+  data TEXT,
+  id_marca INT,
+  vendas INT,
+  valor_do_veiculo INT,
+  nome_veiculo TEXT,
+  marca TEXT
+);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
